fcdd4aaa3490676af7837865bfa9d7f2e2922545
Prasanna-dot <prasannavenkatehs20.01.2k1@gmail.com>
2022-05-31 15:59:12 +0530

    Add

--- Ruby/variables.rb
+++ Ruby/variables.rb
@@ -1,10 +1,23 @@
-$mumber = ""
+$mumber = "Venkatesh" #global variable
 
 class INSTANCEVARIABLE
-    @@No_change = 0
+    @@No_change = 0 #class variable
+    @number = 10 #Instance variable
     
     def Students(name,age)
-        @stu_name=name
+        @stu_name=name #local variable
         @stu_age=age
     end
+
+    def buf
+        puts "Name is #@stu_name"
+        puts "Age is #@stu_age"
+        puts "mumber is #$mumber"
+        puts "change is #@@No_change"
+    end
+    puts "number is #@number"
 end
+
+obj = INSTANCEVARIABLE.new
+obj.Students("Prasanna", 9)
+obj.buf
